c0c5734acccf6a0e9489d757a9800247f8addd6d
mirrormingzZ <mirrorming@qq.com>
2018-12-06 22:35:49 +0800

    Site updated: 2018-12-06 22:35:39

---
+++ dist/music.js
@@ -0,0 +1,32 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    fixed: true,
+    autoplay: false,
+    audio: [
+      {
+        name: "出山",
+        artist: '花粥 / 王胜男',
+        url: 'http://link.hhtjim.com/163/1313354324.mp3',
+        cover: 'http://p2.music.126.net/xUAfdMHdXhu3BmO4g8nOYA==/109951163573311341.jpg',
+      },
+      {
+        name: 'something just like this',
+        artist: '二狗村高富帅',
+        url: 'http://link.hhtjim.com/163/865909031.mp3',
+        cover: 'http://p1.music.126.net/OWfPIjc3RADgfUS3cWXQ7g==/109951163403998980.jpg',
+      },
+      {
+        name: '纸短情长',
+        artist: '花粥',
+        url: 'http://link.hhtjim.com/163/557581284.mp3',
+        cover: 'http://p2.music.126.net/PXE9MfYCgnjHz1vkrpUywQ==/109951163290871736.jpg',
+      },
+      {
+        name: 'Sémillon',
+        artist: '尚先生',
+        url: 'http://link.hhtjim.com/163/452526756.mp3',
+        cover: 'http://p2.music.126.net/hmmH4v6jRNx5b6r7j388uQ==/17763709858639136.jpg',
+      }
+    ]
+});
+
