96d7c809e61546e455079f659d08a2ff79af661b
mirrormingzZ <mirrorming@qq.com>
2018-12-07 21:52:45 +0800

    Site updated: 2018-12-07 21:52:43

--- dist/music.js
+++ dist/music.js
@@ -7,25 +7,37 @@ const ap = new APlayer({
         name: "出山",
         artist: '花粥 / 王胜男',
         url: 'http://link.hhtjim.com/163/1313354324.mp3',
-        cover: 'http://p2.music.126.net/xUAfdMHdXhu3BmO4g8nOYA==/109951163573311341.jpg',
+        cover: 'http://p2.music.126.net/xUAfdMHdXhu3BmO4g8nOYA==/109951163573311341.jpg?param=130y130',
+      },
+      {
+        name: '不染',
+        artist: '毛不易',
+        url: 'http://link.hhtjim.com/163/536099160.mp3',
+        cover: 'http://p2.music.126.net/gEia-o05FSas8uJos54Sug==/109951163456726954.jpg?param=130y130',
+      },
+      {
+        name: '慢慢喜欢你',
+        artist: '莫文蔚',
+        url: 'http://link.hhtjim.com/163/541687281.mp3',
+        cover: 'http://p1.music.126.net/21l0Cy8SO6ycY_eZT-6vVw==/109951163386949943.jpg?param=130y130',
       },
       {
         name: 'something just like this',
         artist: '二狗村高富帅',
         url: 'http://link.hhtjim.com/163/865909031.mp3',
-        cover: 'http://p1.music.126.net/OWfPIjc3RADgfUS3cWXQ7g==/109951163403998980.jpg',
+        cover: 'http://p1.music.126.net/OWfPIjc3RADgfUS3cWXQ7g==/109951163403998980.jpg?param=130y130',
       },
       {
         name: '纸短情长',
         artist: '花粥',
         url: 'http://link.hhtjim.com/163/557581284.mp3',
-        cover: 'http://p2.music.126.net/PXE9MfYCgnjHz1vkrpUywQ==/109951163290871736.jpg',
+        cover: 'http://p2.music.126.net/PXE9MfYCgnjHz1vkrpUywQ==/109951163290871736.jpg?param=130y130',
       },
       {
         name: 'Sémillon',
         artist: '尚先生',
         url: 'http://link.hhtjim.com/163/452526756.mp3',
-        cover: 'http://p2.music.126.net/hmmH4v6jRNx5b6r7j388uQ==/17763709858639136.jpg',
+        cover: 'http://p2.music.126.net/hmmH4v6jRNx5b6r7j388uQ==/17763709858639136.jpg?param=130y130',
       }
     ]
 });
